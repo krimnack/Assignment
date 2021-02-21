aa4687245dfaffc626c64caa5f50cb71bcf912d9
krimnack <nihalmahadik46@gmail.com>
2021-02-21 20:51:12 +0530

    Calc Commit

---
+++ calc.java
@@ -0,0 +1,23 @@
+#import java.util.*
+
+class Calc{
+
+	public static void main(String args[]){
+
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	
+		System.out.println(add(a,b));
+
+	}
+
+	static int add(int a, int b){
+
+		int result = a+b;
+
+		return result;
+	
+	}
+}
