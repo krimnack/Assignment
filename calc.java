007f08c536c41671d3ad55139f2fb64d5d0b868d
krimnack <nihalmahadik46@gmail.com>
2021-02-21 21:27:06 +0530

    Mul function added

--- calc.java
+++ calc.java
@@ -10,12 +10,21 @@ class Calc{
 		int b = sc.nextInt();
 	
 		System.out.println(add(a,b));
+		System.out.println(mul(a,b));
 
 	}
 
 	static int add(int a, int b){
 
 		int result = a+b;
+
+		return result;
+	
+	}
+
+	static int mul(int a, int b){
+
+		int result = a*b;
 
 		return result;
 	
